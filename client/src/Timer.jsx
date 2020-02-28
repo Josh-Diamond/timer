fe3fb696bc5ba99afb050fc59695403e4372099c
Josh Diamond <diamondroad91@gmail.com>
2020-02-28 00:00:56 -0700

    style change

--- client/src/Timer.jsx
+++ client/src/Timer.jsx
@@ -51,16 +51,16 @@ export default class Timer extends Component {
             <div>
                 <h1>{this.state.hours + ' hr ' + ': ' + this.state.minutes + ' min ' + ': ' + this.state.seconds + ' sec '}</h1>
                 <div>
-                    <button onClick={this.start} style={{ margin: '0 3%', height: '30px', width: '75px', fontSize: '1.25rem', letterSpacing: '1.8px', borderRadius: '5px', border: 'none', backgroundColor: '#28A745', color: 'white'}}>Start</button>
-                    <button onClick={this.stop} style={{ margin: '0 3%', height: '30px', width: '75px', fontSize: '1.25rem', letterSpacing: '1.8px', borderRadius: '5px', border: 'none', backgroundColor: '#DC3545', color: 'white'}}>Stop</button>
-                    <button onClick={this.reset} style={{ margin: '0 3%', height: '30px', width: '75px', fontSize: '1.25rem', letterSpacing: '1.8px', borderRadius: '5px', border: 'none', backgroundColor: '#FFC107', color: 'white'}}>Reset</button>
+                    <button onClick={this.start} style={{ margin: '0 3%', height: '30px', width: '75px', fontSize: '1.25rem', letterSpacing: '1.8px', borderRadius: '5px', border: 'none', backgroundColor: '#28A745', color: 'white', outline: 'none', cursor: 'pointer'}}>Start</button>
+                    <button onClick={this.stop} style={{ margin: '0 3%', height: '30px', width: '75px', fontSize: '1.25rem', letterSpacing: '1.8px', borderRadius: '5px', border: 'none', backgroundColor: '#DC3545', color: 'white', outline: 'none', cursor: 'pointer'}}>Stop</button>
+                    <button onClick={this.reset} style={{ margin: '0 3%', height: '30px', width: '75px', fontSize: '1.25rem', letterSpacing: '1.8px', borderRadius: '5px', border: 'none', backgroundColor: '#FFC107', color: 'white', outline: 'none', cursor: 'pointer'}}>Reset</button>
                 </div>
                 <div style={{ marginTop: '3%'}}>
                     <div>
                         <input placeholder='Enter User' id='user' style={{ textAlign: 'center', height: '25px', width: '150px', fontSize: '1.25rem', letterSpacing: '1.8px', margin: '0 1%'}} onChange={this.changeHandler} name='user' value={this.state.user} />
                         <input placeholder='Enter Event' id='event' style={{ textAlign: 'center', height: '25px', width: '150px', fontSize: '1.25rem', letterSpacing: '1.8px', margin: '0 1%'}} onChange={this.changeHandler} name='event' value={this.state.event} />
                     </div>
-                    <button onClick={() => this.save({user: this.state.user, event: this.state.event, hours: this.state.hours, minutes: this.state.minutes, seconds: this.state.seconds})} style={{ marginTop: '2%', height: '30px', width: '75px', fontSize: '1.25rem', letterSpacing: '1.8px', borderRadius: '5px', border: 'none', backgroundColor: '#007BFF', color: 'white'}}>Save</button>
+                    <button onClick={() => this.save({user: this.state.user, event: this.state.event, hours: this.state.hours, minutes: this.state.minutes, seconds: this.state.seconds})} style={{ marginTop: '2%', height: '30px', width: '75px', fontSize: '1.25rem', letterSpacing: '1.8px', borderRadius: '5px', border: 'none', backgroundColor: '#007BFF', color: 'white', outline: 'none', cursor: 'pointer'}}>Save</button>
                 </div>
                 <div style={{ textAlign: 'left', marginLeft: '5%', marginRight: '5%'}}>
                     <h2>Time Sheet</h2>
